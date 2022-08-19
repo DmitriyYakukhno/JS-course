da40b0a8a64bbc6d5f3549daf92d5123262a4b44
Dmytro Yakukhno <DmytroYakukhno@gmail.com>
2022-08-19 12:33:21 +0300

    modernize app

--- index.js
+++ index.js
@@ -8,10 +8,24 @@ const personalMovieDB = {
   private: false
 };
 
-const a = prompt("Name one of the recent movies you saw:", ""),
-      b = +prompt("Rate this movie from 1 to 5:"),
-      c = prompt("Name one of the recent movies you saw:", ""),
-      d = +prompt("Rate this movie from 1 to 5:");
+loop: for (let i = 0; i < 2; i++) {
+  const movie = prompt("Name one of the recent movies you saw:", "");
+  const rate = +prompt("Rate this movie from 1 to 5:");
+  
+  if (movie != null && rate != null && movie != '' && rate != '' && movie.length < 50) {
+    personalMovieDB.movies[movie] = rate;
+  } else {
+    i--;
+  }
+}
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+if (personalMovieDB.count < 10) {
+  console.log(`You saw small number of movies`);
+} else if (personalMovieDB.count > 10 && personalMovieDB.count < 30) {
+  console.log(`You are classic audience`);
+} else {
+  console.log(`You are fun of movies`);
+}
+
+
+
