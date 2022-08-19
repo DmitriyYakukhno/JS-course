99c687de5572f5753aef7e7e211e7c0ddf38987b
Dmytro Yakukhno <DmytroYakukhno@gmail.com>
2022-08-19 10:14:55 +0300

    first app

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
-let arr = [1, 2, 3];
-console.log(arr[1]);
+const numberOfFilms = +prompt("How many movies did you alredy saw?", "");
+
+const personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  private: false
+};
+
+const a = prompt("Name one of the recent movies you saw:", ""),
+      b = +prompt("Rate this movie from 1 to 5:"),
+      c = prompt("Name one of the recent movies you saw:", ""),
+      d = +prompt("Rate this movie from 1 to 5:");
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
